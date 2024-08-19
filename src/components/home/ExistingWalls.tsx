4b2ccbc6691e7bc57222b30c8b93c270f89443f1
JuvanH <juvanholder09@gmail.com>
2024-08-19 20:14:16 +0200

    Fix for Heroku deployment

--- src/components/home/ExistingWalls.tsx
+++ src/components/home/ExistingWalls.tsx
@@ -37,7 +37,7 @@ const ExistingWalls = () => {
                   width="100%"
                   height="100%"
                   style={{border:0}}
-                  allowFullScreen=""
+                  allowFullScreen={true}
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
